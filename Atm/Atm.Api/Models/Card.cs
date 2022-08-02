64413cdfb8ce72b17ebaf2a453d7a4e11174f177
Igor Bihanskyi <IgorBihanskyi@gmail.com>
2022-08-02 16:27:21 +0300

    fix

--- Atm/Atm.Api/Models/Card.cs
+++ Atm/Atm.Api/Models/Card.cs
@@ -1,10 +1,14 @@
-﻿namespace Atm.Api.Models
+﻿using Newtonsoft.Json.Converters;
+using System.Text.Json.Serialization;
+
+namespace Atm.Api.Models
 {
     public class Card
     {
         public string CardNumber { get; set; }
         public string FullName { get; set; }
         public string Password { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public CardBrands CardBrand { get; set; }
         public int Money { get; set; }
     }
