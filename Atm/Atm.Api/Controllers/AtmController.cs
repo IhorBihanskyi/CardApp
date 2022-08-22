9e2bf12d78f93306c67da34f3c08259cba244894
Igor Bihanskyi <IgorBihanskyi@gmail.com>
2022-08-22 12:37:17 +0300

    Code refactoring

--- Atm/Atm.Api/Controllers/AtmController.cs
+++ Atm/Atm.Api/Controllers/AtmController.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Atm.Api.Controllers.Requests;
 using Atm.Api.Controllers.Responses;
-using Microsoft.Extensions.Caching.Memory;
-using Atm.Api.Models;
 
 namespace Atm.Api.Controllers;
 
@@ -11,69 +9,43 @@ namespace Atm.Api.Controllers;
 [Route("/api/[controller]/cards/")]
 public class AtmController : ControllerBase
 {
-    private const string initKey = "init";
-    private const string authorizeKey = "author";
     private readonly IAtmService _atmService;
     private readonly IBankService _bankService;
-    private IMemoryCache _cache;
 
-    public AtmController(IAtmService atmService, IBankService bankService, IMemoryCache cache)
+    public AtmController(IAtmService atmService, IBankService bankService)
     {
         _atmService = atmService;
         _bankService = bankService;
-        _cache = cache;
     }
 
     [HttpGet("{cardNumber}/init")]
     public IActionResult Init(string cardNumber)
     {
         return _bankService.IsCardExist(cardNumber)
-            ? Ok(new AtmResponse($"Your card {_cache.Set(initKey, cardNumber)} in the system!"))
+            ? Ok(new AtmResponse($"Your card in the system!"))
             : NotFound(new AtmResponse("Your card isn't in the system!"));
     }
 
     [HttpPost("authorize")]
     public IActionResult Authorize([FromBody] CardAuthorizeRequest request)
     {
-        string token = string.Empty;
-        if (_cache.TryGetValue(initKey, out token))
-        {
-            _cache.Remove(initKey);
-
-            return _bankService.VerifyCardPassword(request.CardNumber, request.CardPassword)
-            ? Ok(new AtmResponse($"{_cache.Set(authorizeKey, request.CardPassword)} Authorization was successfully!"))
-            : Unauthorized(new AtmResponse("Invalid password"!));
-        }
-        return BadRequest();
-            
+        return _bankService.VerifyCardPassword(request.CardNumber, request.CardPassword)
+        ? Ok(new AtmResponse($" Authorization was successfully!"))
+        : Unauthorized(new AtmResponse("Invalid password"!));
     }
 
     [HttpPost("withdraw")]
     public IActionResult Withdraw([FromBody] CardWithdrawRequest request)
     {
-        string token = string.Empty;
-        if (_cache.TryGetValue(authorizeKey, out token))
-        {
-            _cache.Remove(authorizeKey);
-
-            _atmService.Withdraw(request.CardNumber, request.Amount);
-            return Ok(new AtmResponse("The operation was successfully!"));
-        }
-        return BadRequest();
+        _atmService.Withdraw(request.CardNumber, request.Amount);
+        return Ok(new AtmResponse("The operation was successfully!"));
     }
 
     [HttpGet("{cardNumber}/balance")]
     public IActionResult GetBalance(string cardNumber)
     {
-        string token = string.Empty;
-        if (_cache.TryGetValue(authorizeKey, out token))
-        {
-            _cache.Remove(authorizeKey);
+        var balance = _bankService.GetCardBalance(cardNumber);
+        return Ok(new AtmResponse($"Balance is {balance}"));
 
-            var balance = _bankService.GetCardBalance(cardNumber);
-            return Ok(new AtmResponse($"Balance is {balance}"));
-        }
-        return BadRequest();
-        
     }
 }
--- Atm/Atm.Api/Services/BankService.cs
+++ Atm/Atm.Api/Services/BankService.cs
@@ -1,5 +1,6 @@
 ﻿using Atm.Api.Interfaces;
 using Atm.Api.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Atm.Api.Services;
 public class BankService : IBankService
@@ -7,34 +8,73 @@ public class BankService : IBankService
     public const int VisaLimit = 200;
     public const int MasterCardLimit = 300;
 
+    private IMemoryCache _cache;
+    private const string initKey = "init";
+    private const string authorizeKey = "author";
+
+    public BankService( IMemoryCache cache)
+    {
+        _cache = cache;
+    }
+
     private static readonly IReadOnlyCollection<Card> Cards = new List<Card>
     {
         new ("4444333322221111", "Troy Mcfarland","edyDfd5A", CardBrands.Visa, 800),
         new ("5200000000001005", "Levi Downs", "teEAxnqg", CardBrands.MasterCard, 400)
     };
 
-    public bool IsCardExist(string cardNumber) => Cards.Any(x => x.Number == cardNumber);
+    public bool IsCardExist(string cardNumber) 
+    {
+        if(Cards.Any(x => x.Number == cardNumber))
+        {
+            _cache.Set(initKey, cardNumber);
+            return true;
+        }
+        throw new ArgumentOutOfRangeException("Not allowed operation!");
+    }
 
     public Card GetCard(string cardNumber) => Cards.Single(x => x.Number == cardNumber);
 
-    public int GetCardBalance(string cardNumber)
-        => GetCard(cardNumber)
-            .GetBalance();
-
     public bool VerifyCardPassword(string cardNumber, string cardPassword)
-        => GetCard(cardNumber)
-            .IsPasswordEqual(cardPassword);
+    {
+        string token = string.Empty;
+        if (_cache.TryGetValue(initKey, out token) && GetCard(cardNumber).IsPasswordEqual(cardPassword))
+        {
+            _cache.Remove(initKey);
+            _cache.Set(authorizeKey, cardPassword);
+            return true;
+        }
+        throw new ArgumentOutOfRangeException("Not allowed operation!");
+    }
+
+    public int GetCardBalance(string cardNumber)
+    {
+        string token = string.Empty;
+        if (_cache.TryGetValue(authorizeKey, out token))
+        {
+            _cache.Remove(authorizeKey);
+            return GetCard(cardNumber).GetBalance();
+        }
+        throw new ArgumentOutOfRangeException("Not allowed operation!");
+    }
 
     public bool VerifyCardLimit(string cardNumber, int amount)
     {
         var card = GetCard(cardNumber);
-        return (card.CardBrand, amount) switch
+
+        string token = string.Empty;
+        if (_cache.TryGetValue(authorizeKey, out token))
         {
-            { CardBrand: CardBrands.Visa, amount: > VisaLimit } => 
-                throw new InvalidOperationException($"One time {card.CardBrand} withdraw limit is {VisaLimit}"),
-            { CardBrand: CardBrands.MasterCard, amount: > MasterCardLimit } => 
-                throw new InvalidOperationException($"One time {card.CardBrand} withdraw limit is {MasterCardLimit}"),
-            _ => true
-        };
+            _cache.Remove(authorizeKey);
+            return (card.CardBrand, amount) switch
+            {
+                { CardBrand: CardBrands.Visa, amount: > VisaLimit } =>
+                    throw new InvalidOperationException($"One time {card.CardBrand} withdraw limit is {VisaLimit}"),
+                { CardBrand: CardBrands.MasterCard, amount: > MasterCardLimit } =>
+                    throw new InvalidOperationException($"One time {card.CardBrand} withdraw limit is {MasterCardLimit}"),
+                _ => true
+            };
+        }
+        throw new ArgumentOutOfRangeException("Not allowed operation!");
     }
 }
