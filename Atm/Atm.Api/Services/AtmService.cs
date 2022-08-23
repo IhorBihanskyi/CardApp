a597502a1fa8c0d6f17230d98a0abfbfc9234ef4
Igor Bihanskyi <IgorBihanskyi@gmail.com>
2022-08-23 14:01:25 +0300

    Refactoring

--- Atm/Atm.Api/Services/AtmService.cs
+++ Atm/Atm.Api/Services/AtmService.cs
@@ -1,15 +1,19 @@
 ﻿using Atm.Api.Interfaces;
+using Atm.Api.Models;
 
 namespace Atm.Api.Services;
 
 public sealed class AtmService : IAtmService
 {
     private readonly IBankService _bankService;
+    private IAtmEventBroker _broker;
+
     private int TotalAmount { get; set; } = 10_000;
 
-    public AtmService(IBankService bankService)
+    public AtmService(IBankService bankService, IAtmEventBroker broker)
     {
         _bankService = bankService;
+        _broker = broker;
     }
 
     public void Withdraw(string cardNumber, int amount)
@@ -24,6 +28,12 @@ public sealed class AtmService : IAtmService
             throw new ArgumentOutOfRangeException("Insufficient funds at the ATM!");
         }
 
+        if (_broker.GetLastEvent(cardNumber) is not AuthorizeEvent)
+        {
+            throw new InvalidOperationException("Pass identification and authorization!");
+        }
+        _broker.AppendEvent(cardNumber, new WithDrawEvent());
+
         _bankService.Withdraw(cardNumber, amount);
 
         TotalAmount -= amount;
@@ -31,16 +41,31 @@ public sealed class AtmService : IAtmService
 
     public bool IsCardExist(string cardNumber)
     {
-        return _bankService.IsCardExist(cardNumber);
+        if (_bankService.IsCardExist(cardNumber))
+        {
+            _broker.StartStream(cardNumber, new InitEvent());
+            return true;
+        }
+        throw new InvalidOperationException("Pass identification and authorization!");
     }
 
     public bool VerifyPassword(string cardNumber, string cardPassword)
     {
-        return _bankService.VerifyPassword(cardNumber, cardPassword);
+        if (_broker.FindEvent<InitEvent>(cardNumber) is not null && _bankService.VerifyPassword(cardNumber, cardPassword))
+        {
+            _broker.AppendEvent(cardNumber, new AuthorizeEvent());
+            return true;
+        }
+        throw new InvalidOperationException("Pass identification and authorization!");
     }
 
     public int GetCardBalance(string cardNumber)
     {
+        if (_broker.GetLastEvent(cardNumber) is not AuthorizeEvent)
+        {
+            throw new InvalidOperationException("Pass identification and authorization!");
+        }
+        _broker.AppendEvent(cardNumber, new BalanceEvent());
         return _bankService.GetCardBalance(cardNumber);
     }
 }
--- Atm/Atm.Api/Services/BankService.cs
+++ Atm/Atm.Api/Services/BankService.cs
@@ -7,13 +7,6 @@ public class BankService : IBankService
     public const int VisaLimit = 200;
     public const int MasterCardLimit = 300;
 
-    private IAtmEventBroker _broker;
-
-    public BankService(IAtmEventBroker broker)
-    {
-        _broker = broker;
-    }
-
     private static readonly IReadOnlyCollection<Card> Cards = new List<Card>
     {
         new ("4444333322221111", "Troy Mcfarland","edyDfd5A", CardBrands.Visa, 800),
@@ -31,33 +24,16 @@ public class BankService : IBankService
         return WithdrawLimits.First(x => x.CardBrand == cardBrand).Amount;
     }
 
-    public bool IsCardExist(string cardNumber)
-    {
-        if (Cards.Any(x => x.Number == cardNumber))
-        {
-            _broker.StartStream(cardNumber, new InitEvent());
-            return true;
-        }
-        throw new InvalidOperationException("Pass identification and authorization!");
-    }
+    public bool IsCardExist(string cardNumber) => Cards.Any(x => x.Number == cardNumber);
+    
 
     public bool VerifyPassword(string cardNumber, string cardPassword)
     {
-        if (_broker.FindEvent<InitEvent>(cardNumber) is not null && GetCard(cardNumber).IsPasswordEqual(cardPassword))
-        {
-            _broker.AppendEvent(cardNumber, new AuthorizeEvent());
-            return true;
-        }
-        throw new InvalidOperationException("Pass identification and authorization!");
+        return GetCard(cardNumber).IsPasswordEqual(cardPassword);
     }
 
     public int GetCardBalance(string cardNumber)
     {
-        if (_broker.GetLastEvent(cardNumber) is not AuthorizeEvent)
-        {
-            throw new InvalidOperationException("Pass identification and authorization!");
-        }
-        _broker.AppendEvent(cardNumber, new BalanceEvent());
         return GetCard(cardNumber).GetBalance();
     }
 
@@ -72,11 +48,7 @@ public class BankService : IBankService
         {
             throw new InvalidOperationException($"One time {card.CardBrand} withdraw limit is {limit}");
         }
-        if (_broker.GetLastEvent(cardNumber) is not AuthorizeEvent)
-        {
-            throw new InvalidOperationException("Pass identification and authorization!");
-        }
-        _broker.AppendEvent(cardNumber, new WithDrawEvent());
+        
         card.Withdraw(amount);
     }
 }
